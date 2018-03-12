272430b7e5f24dbb5f4452f8bff32cffad36b924
Cesar Riello <czaredesign@gmail.com>
2018-03-11 23:01:51 -0300

    add: app info and icon

--- App.js
+++ App.js
@@ -1,47 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, StatusBar } from 'react-native';
-import { TabNavigator } from 'react-navigation'
-import { Constants } from 'expo'
+import Main from './containers/Main'
 
-const TabsNavigation = TabNavigator({
-  DecksList: {
-    screen: DecksList,
-    navigationOptions: {
-      tabBarLabel: 'Decks List',
-    }
-  },
-  NewDeck: {
-    screen: NewDeck,
-    navigationOptions: {
-      tabBarLabel: 'Create New Deck',
-    }
-  }
-},{
-  animationEnabled: true,
-  tabBarPosition: 'top',
-  tabBarOptions: {
-    activeBackgroundColor: `#f00`,
-    activeTintColor: '#fff',
-    style: { backgroundColor: '#0f0'}
-  }
-})
-
-
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
-    return (
-      <View style={{flex: 1}}>
-        <View style={{ backgroundColor: '#eeeeee', height: Constants.statusBarHeight }}>
-          <StatusBar translucent backgroundColor={'#cceeee'} />
-        </View>
-        <TabsNavigator />
-      </View>
-    );
+    return <Main />
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#eeeeee'
-  },
-});
+export default App
