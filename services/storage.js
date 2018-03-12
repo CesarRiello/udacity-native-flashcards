270742c2f4cf9bdd408c5a591dbcba02b91cb790
Cesar Riello <czaredesign@gmail.com>
2018-03-11 22:32:03 -0300

    refactor: use a single storage service

---
+++ services/storage.js
@@ -0,0 +1,106 @@
+import { AsyncStorage } from 'react-native'
+import uuidv1 from 'uuid/v1'
+import {
+  Permissions,
+  Notifications
+} from 'expo'
+
+const FLASHCARDS_KEY = 'FlashCards:cards'
+const NOTIFICATION_KEY = 'FlashCards:notifications'
+
+const standartNotification = {
+  title: 'don\'t forget to study',
+  body: "Use the flashcards to study",
+  ios: { sound: true },
+  android: {
+    priority: 'high',
+    sound: true,
+    vibrate: true
+  }
+}
+
+export const decks = {
+  list: () => {
+    return AsyncStorage.getItem(FLASHCARDS_KEY)
+  },
+  get: ({key}) => {
+    return AsyncStorage.getItem(FLASHCARDS_KEY).then(decks => {
+      decks = JSON.parse(decks) || []
+      return decks.find(deck => deck.key === key)
+    })
+  },
+  add: ({title}) => {
+    return AsyncStorage.getItem(FLASHCARDS_KEY)
+      .then(decks => {
+        deckList = JSON.parse(decks) || []
+        const deckAdd = {
+          title,
+          key: uuidv1(),
+          questions: []
+        }
+        const deckAddList = [...deckList, deckAdd]
+        AsyncStorage.setItem(FLASHCARDS_KEY, JSON.stringify(deckAddList))
+        return deckAdd
+      })
+  },
+  remove: ({key}) => {
+    return AsyncStorage.getItem(FLASHCARDS_KEY)
+      .then(decks => {
+        deckList = JSON.parse(decks) || []
+        const deckAddList = (deckList || [])
+          .filter(deck => {
+            return deck.key !== key
+        })
+        AsyncStorage.setItem(FLASHCARDS_KEY, JSON.stringify(deckAddList))
+        return deckAddList
+      })
+  }
+}
+
+export const cards = {
+  add: ({key, question}) => {
+    return AsyncStorage.getItem(FLASHCARDS_KEY).then(decks => {
+      const deckList = JSON.parse(decks) || []
+      const deckAddList = deckList.map(deck => {
+        if(deck.key === key) {
+          deck.questions = [...deck.questions, question]
+        }
+        return deck
+      })
+      AsyncStorage.setItem(FLASHCARDS_KEY, JSON.stringify(deckAddList))
+    })
+  }
+}
+
+export const notifications = {
+  add: () => {
+    AsyncStorage.getItem(NOTIFICATION_KEY)
+      .then(JSON.parse)
+      .then((data) => {
+        if (data === null) {
+          Permissions.askAsync(Permissions.NOTIFICATIONS)
+            .then(({ status }) => {
+              if (status === 'granted') {
+                Notifications.cancelAllScheduledNotificationsAsync()
+
+                let tomorrow = new Date()
+                tomorrow.setDate(tomorrow.getDate() + 1)
+                tomorrow.setHours(20)
+                tomorrow.setMinutes(0)
+
+                Notifications.scheduleLocalNotificationAsync(
+                  standartNotification,
+                  { time: tomorrow, repeat: 'day' }
+                )
+
+                AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+              }
+            })
+        }
+      })
+  },
+  clear: () => {
+    return AsyncStorage.removeItem(NOTIFICATION_KEY)
+      .then(Notifications.cancelAllScheduledNotificationsAsync)
+  }
+}
--- services/theme.js
+++ services/theme.js
@@ -1,8 +1,12 @@
-
 export const colors = {
-  incorrect: '#ee1100',
-  correct: '#2200ee',
-  feature: '#ff00ee',
-  primary: '#222222',
-  background: '#f9f9f9'
+    incorrect: '#ee1100',
+    correct: '#2200ee',
+    feature: '#ff00ee',
+    primary: '#dd2200',
+    secondary: '#3300dd',
+    gray: '#777777',
+    silver: '#eeeeee',
+    light: '#f6f6f6',
+    white: '#ffffff',
+    background: '#f9f9f9'
 }
